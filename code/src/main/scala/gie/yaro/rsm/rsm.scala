69541cd0f3a503e0ee3e3d260bd37a431ff9bcf0
Igor Gorbunov <igormbox@gmail.com>
2015-08-27 23:02:04 +0300

    rsm parsing defined for 1.4

--- code/src/main/scala/gie/yaro/rsm/rsm.scala
+++ code/src/main/scala/gie/yaro/rsm/rsm.scala
@@ -2,9 +2,9 @@ package gie.yaro.rsm.file
 
 import gie.yaro.RoStore
 import scodec.bits.{ByteVector, BitVector}
-import scodec.{codecs, Codec}
+import scodec._
 
-import codecs._
+import scodec.codecs._
 import implicits._
 import slogging.LazyLogging
 
@@ -36,6 +36,21 @@ object codec extends LazyLogging {
   }
 
 
+  class FixedVectorCodec[A](count: Int, codec: Codec[A]) extends Codec[Vector[A]] {
+
+    def sizeBound = codec.sizeBound * count
+
+    def encode(vector: Vector[A]) = Encoder.encodeSeq(codec)(vector)
+
+    def decode(buffer: BitVector) = Decoder.decodeCollect[Vector, A](codec, Some(count))(buffer)
+
+    override def toString = s"vector($codec)"
+
+  }
+
+  def fixedVectorCodec[A](count: Int, codec: Codec[A]):Codec[Vector[A]] = new FixedVectorCodec(count, codec)
+
+
   val rsmMagic = {
     "rsm_magic" | (constant('G') ~> constant('R') ~> constant('S') ~> constant('M'))
   }
@@ -64,6 +79,16 @@ object codec extends LazyLogging {
     ???
   }
 
+  def emptyStringToOpt(c: Codec[String]):Codec[Option[String]] = {
+    c.xmap(
+      str=>if(str.isEmpty) None else Some(str),
+      _ match {
+          case Some(str) => str
+          case None => ""
+      }
+    )
+  }
+
   val header ={
     rsmMagic ~> version :: animationLength :: shaderType :: alphaValue :: reserved
   }.as[Header]
@@ -73,8 +98,56 @@ object codec extends LazyLogging {
 
   }
 
+  val mainNodeName ={
+    ("main_node_name" | fixedString(40))
+  }
+
+  val textureId = ("texture_id" | int32L)
+
+  val texColor = { int32L.withContext("color") :: floatL.withContext("u") :: floatL.withContext("v") }
+
+  val face = {
+    ("vertex_idx" | int16L :: int16L :: int16L) ::
+    ("texture_vertex_idx" | int16L :: int16L :: int16L) ::
+    ("texture_id" | int16L) ::
+    ("m_reserved_padding" | int16L) ::
+    ("two_side" | int32L) ::
+    ("smooth_group" | int32L)
+  }
+
+  val rotationalKeyFrame = {
+    ("frame" | int32L) ::
+    ("qx" | floatL) ::
+    ("qy" | floatL) ::
+    ("qz" | floatL) ::
+    ("qw" | floatL)
+  }
+
+  val node ={
+    ("node_name" | fixedString(40)) ::
+    ("parent_node_name" | emptyStringToOpt(fixedString(40))) ::
+    ("textures_id" | vectorOfN( int32L.withContext("number_of_bound_textures"), textureId)) ::
+    ("axis_origin" | fixedVectorCodec(3*4, floatL) ) :: //3x4 matrix that identifies the axis and origin of this node
+    ("translation" | fixedVectorCodec(3, floatL) ) ::
+    ("rot_angle" | floatL ) :: // angle of rotation around the axis in radians
+    ("rot_axis" | fixedVectorCodec(3, floatL) ) ::
+    ("scale" | fixedVectorCodec(3, floatL) ) ::
+    ("vertices" | vectorOfN( int32L.withContext("num_of_vertices"), (floatL :: floatL :: floatL))) ::
+    ("texture_coordinates" | vectorOfN( int32L.withContext("num_of_tex_coords"), texColor)) ::
+    ("faces" | vectorOfN( int32L.withContext("num_of_faces"), face)) ::
+      /*:: positionla animation if version >=1.5*/
+    ("rotational_animation" | vectorOfN( int32L.withContext("num_of_key_frames"), rotationalKeyFrame))
+  }
+
+  val nodes ={
+    ("nodes" | vectorOfN(int32L, node))
+  }
+
   val file = {
-    header :: textureNames
+    header ::
+    textureNames ::
+    mainNodeName ::
+    nodes
   }
 
 
@@ -82,18 +155,9 @@ object codec extends LazyLogging {
   def test()(implicit executor: ExecutionContext): Unit = {
     logger.debug("codec.test()")
 
-    val iconv = gie.Require.require("iconv-lite")
-
-    println(s">>> ${iconv}")
-
     async {
 
-      //gie.codeset.TableMap.parse( await(RoStore.open("ro-data-unpacked/KOREAN.TXT")) )
-
-       //gie.codeset.TableMap.parse( await(RoStore.open("ro-data-unpacked/test.txt")) )
-
       val r = file.decode( BitVector( await( RoStore.open("ro-data-unpacked/model/글래지하수로/하수구_라이온1.rsm") ) ) )
-
       println(s"result >> ${r}")
 
     }.onComplete( _.get )
