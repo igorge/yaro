fa0c89169468d600625bd3fcc2a881d61c54b315
Igor Gorbunov <igormbox@gmail.com>
2015-09-21 19:35:12 +0300

    ...

--- code/src/main/scala/gie/gsg/attributes/vertex_attribute_attribute.scala
+++ code/src/main/scala/gie/gsg/attributes/vertex_attribute_attribute.scala
@@ -6,12 +6,18 @@ import gie.gsg.RenderContext
 trait VertexAttributeAttributeComponent {
   this: RenderContext with StateAttributeComponent with GlProgramComponent with ShaderVariableComponent  =>
 
-  class VertexAttributeAttribute(val vertexAttr:gl.VertexAttribute, bufferFun: ()=>gl.GLBuffer, val componentSize: Int, componentType: Int, stride: Int, offset: Int) extends ShaderVariableAttribute {
+  class VertexAttributeAttribute(val vertexAttr:gl.VertexAttribute, bufferFun: ()=>gl.GLBuffer, val componentSize: Int, val componentType: Int, val stride: Int, val offset: Int) extends ShaderVariableAttribute {
     lazy val buffer = bufferFun()
     def name = vertexAttr.name
 
     def apply(from: ShaderVariableAttribute): Unit={
-      vertexAttr.bindBuffer(buffer).vertexAttribPointer(componentSize, componentType, true, stride, offset).enable()
+      if(from eq null) {
+        vertexAttr.bindBuffer(buffer).vertexAttribPointer(componentSize, componentType, true, stride, offset).enable()
+      } else {
+        val fromT = from.asInstanceOf[VertexAttributeAttribute]
+        if(buffer != fromT.buffer) vertexAttr.bindBuffer(buffer)
+        vertexAttr.vertexAttribPointer(componentSize, componentType, true, stride, offset).enable()
+      }
     }
     def unapply(): Unit={
       vertexAttr.disable()
@@ -23,13 +29,21 @@ trait VertexAttributeAttributeComponent {
 
       if (name != y.name) {
         false
+      } else if (this eq y) {
+        true
       } else {
         val yTyped = y.asInstanceOf[VertexAttributeAttribute]
-        vertexAttr eq yTyped.vertexAttr
+
+        (vertexAttr eq yTyped.vertexAttr) &&
+          (buffer == yTyped.buffer) &&
+          (componentSize == yTyped.componentSize) &&
+          (componentType == yTyped.componentType) &&
+          (stride == yTyped.stride) &&
+          (offset == yTyped.stride)
       }
     }
 
-    override def toString = s"VertexAttributeAttribute(${name})"
+    override def toString = s"VertexAttributeAttribute(name=${name}, componentSize=${componentSize}, componentType=${componentType}, stride=${stride}, offset=${offset})"
 
   }
 }
